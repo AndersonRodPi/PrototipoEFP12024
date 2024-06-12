ab638fbfcdbb4087aef9f47569c5757ce8035f21
Anderson Rodríguez <arodriguezp16@miumg.edu.gt>
2024-06-12 10:04:12 -0600

    Base del programa

---
+++ PrototipoEFP12024/include/cliente.h
@@ -0,0 +1,16 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+
+class cliente
+{
+    public:
+        cliente();
+        virtual ~cliente();
+
+    protected:
+
+    private:
+};
+
+#endif // CLIENTE_H
